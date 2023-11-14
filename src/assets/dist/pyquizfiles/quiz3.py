cbb1060b9fa7ea834673bdc4e2c5de59a9574954
Jaydeep Karale <tech.09.life@gmail.com>
2023-11-14 22:14:06 +0530

    uload quiz apps

---
+++ src/assets/dist/pyquizfiles/quiz3.py
@@ -0,0 +1,13 @@
+class MyClass:
+    def __init__(self, my_var):
+        self.my_var = my_var
+    
+    def instance_method(self):
+        self.my_var  = 30
+        return f"{self.my_var}"
+    
+
+obj = MyClass(10)
+obj.instance_method()
+
+print(obj.my_var)
